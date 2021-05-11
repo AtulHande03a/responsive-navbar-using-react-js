b6d0a56bc20a9a1419cbad4fb339c8f2ddca5f07
Atul Kumar Hande <handeatul03a@gmail.com>
2021-05-12 00:29:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import ReorderIcon from "@material-ui/icons";
+import SearchIcon from "@material-ui/icons";
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  return (
+    <div className="Navbar">
+      <div className="leftSide">
+        <div className="links" id={showLinks ? "hidden" : ""}>
+          <a href="./home">Home</a>
+          <a href="./feedback">Feedback</a>
+          <a href="./aboutus">About Us</a>
+          <a href="./contact">Contact</a>
+        </div>
+        <button className="btn" onClick={() => setShowLinks(!showLinks)}>
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="rightSide">
+        <input type="text" placeholder="Search.." />
+        <button>Search</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
